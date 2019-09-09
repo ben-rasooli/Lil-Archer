5e1846d61b27d1a815c6481ec0a21b2f24b3d409
Troy <troyrooney39@gmail.com>
2019-09-09 16:36:35 +1000

    Added Pause Menu

---
+++ Lil Archer/Assets/_Project/PauseMenu/PauseButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+
+    [SerializeField] Button m_button;
+    [SerializeField] GameObject m_PauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+    public void OnClick()
+    {
+        m_PauseMenu.SetActive(true);
+    }
+}
